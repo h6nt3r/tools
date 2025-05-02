28704467049a672c281b144ca6ea5b1ac2a4fbe2
Mijanur Rahman <mijanurrahman.rubel2@gmail.com>
2025-05-02 21:04:51 +0600

    Add files via upload

--- oneliners/sqlier.sh
+++ oneliners/sqlier.sh
@@ -147,7 +147,7 @@ if [[ "$1" == "-d" && "$3" == "-all" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=BEST --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -170,7 +170,7 @@ if [[ "$1" == "-d" && "$3" == "-e" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=E --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -193,7 +193,7 @@ if [[ "$1" == "-d" && "$3" == "-b" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=B --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -216,7 +216,7 @@ if [[ "$1" == "-d" && "$3" == "-bb" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=BB --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -240,7 +240,7 @@ if [[ "$1" == "-d" && "$3" == "-t" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=T --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -269,7 +269,7 @@ if [[ "$1" == "-l" && "$3" == "-all" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=BEST --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -298,7 +298,7 @@ if [[ "$1" == "-l" && "$3" == "-t" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=T --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -328,7 +328,7 @@ if [[ "$1" == "-l" && "$3" == "-b" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=B --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -357,7 +357,7 @@ if [[ "$1" == "-l" && "$3" == "-e" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=E --random-agent --confirm --force-ssl --dbs --batch
     exit 0
@@ -386,7 +386,7 @@ if [[ "$1" == "-l" && "$3" == "-bb" ]]; then
 
     cat $base_dir/urlfinder.txt $base_dir/gau.txt $base_dir/waybackurls.txt | sed 's/:[0-9]\+//' | anew | tee $base_dir/all_urls.txt
 
-    cat $base_dir/all_urls.txt | grep -a "[=&]" | grep -aiEv "\.(css|ico|woff|woff2|svg|ttf|eot|png|jpg|js|json|pdf|gif|xml|webp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
+    cat $base_dir/all_urls.txt | grep -Eia "\.(php|asp|aspx|cfm|jsp)($|\s|\?|&|#|/|\.)" | anew | tee $base_dir/all_params_urls.txt
 
     ghauri -m $base_dir/all_params_urls.txt --ignore-code=401 --level=3 --technique=BB --random-agent --confirm --force-ssl --dbs --batch
     exit 0
